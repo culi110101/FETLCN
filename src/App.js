ea57a6e874566f80d61c3e6e069637eef69c5564
culi110101 <buidinhxuanit@gmail.com>
2022-12-03 17:37:22 +0700

    update page 404

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchList from './views/SearchList';
 import JobItemsLayer from './views/JobItemsLayer';
 import ProfileUser from './views/ProfileUser';
 import ManageForCustomer from './views/ManageForCustomer';
+import Page404 from './views/Page404';
 import UpdateInformation from './components/users/UpdateInformation';
 import './assets/sass/main.scss'
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/profilefreelancer' element={<ProfileUser></ProfileUser>}/>
         <Route path='/manageforcustomer' element={<ManageForCustomer></ManageForCustomer>}/>
         <Route path='/updateprofile' element={<UpdateInformation></UpdateInformation>}/>
+        <Route path='/page404' element={<Page404></Page404>}/>
         <Route path='*' element={<Auth></Auth>}/>
       </Routes>
     </BrowserRouter>
